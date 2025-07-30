81ee21442478a74333bf448ba0f1a9687a549105
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-07-30 12:59:30 +0500

    create a server enviroment

---
+++ server/app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.use((req, res) => {
+    console.log('This is a middleware of the website')
+})
+app.listen(5000, () => console.log('Server running on port 5000'));
