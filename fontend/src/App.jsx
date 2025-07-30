2b7b0f769ce5d65f0a1edce2637888bf600a1110
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-07-30 10:52:36 +0500

    theird task of the web development. making a blog website

---
+++ fontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-red-500">
+        this is a blog page of the website
+      </div>
+    </>
+  )
+}
+
+export default App
