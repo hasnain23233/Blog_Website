be18788f892a9b9b685a7672c81a88268896935d
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-08-04 12:03:01 +0500

    responsive the newBloh.jsx page

--- fontend/src/mainPages/NewBlog.jsx
+++ fontend/src/mainPages/NewBlog.jsx
@@ -12,7 +12,7 @@ const NewBlog = () => {
             <h1 className='font-semibold text-2xl tracking-wider'>
                 Build your community, create a blog.
             </h1>
-            <div className='h-20 border mt-5 px-4 border-gray-700 flex gap-4 hover:border-white items-center rounded-xl w-3/12'>
+            <div className='h-20 border mt-5 px-4 border-gray-700 flex gap-4 hover:border-white items-center rounded-xl xl:w-3/12 lg:w-5/12 sm:w-7/12 w-11/12'>
                 <p className='w-14 h-14 bg-[#0d2619] text-green-600 rounded-md flex items-center justify-center'>
                     <FontAwesomeIcon icon={faBlog} size='2x' />
                 </p>
